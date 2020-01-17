114deba46575f70a94a3cbe08759608af855302c
Chuck Francis <bnsf5622@gmail.com>
2020-01-17 13:35:19 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FruitsBasket1._1.sln
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            String[] = { "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Orange", "Orange", "Apple", "O" +
+"range", "Orange", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple", "Apple", "Apple",
+  "Orange", "Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Apple", "Ora" +
+  "nge", "Orange", "Apple", "Orange", "Orange", "Apple" "Apple", "Orange", "Orange", "Apple", "Orange" };
+
+            // Fruit sorting code goes here!
+            
+            
+
+            
+        }
+    }
+}
